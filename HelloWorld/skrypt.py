20c404e1c2ed1c177332c2421ddcae19c2af180e
Gosia <malgosza.niewiadomska@gmail.com>
2018-03-10 19:21:21 +0100

    classa pieska

--- HelloWorld/skrypt.py
+++ HelloWorld/skrypt.py
@@ -1,5 +1,18 @@
 import unittest
 
+# prosta klasa
+class piesek:
+    # konstruktor
+    def __init__(self, imiePieska):
+        self.imie = imiePieska
+
+    def metoda(self):
+        print("piesek ma na imie " + self.imie)
+
+# uzycie
+# p = piesek("azor")
+# p.metoda()
+
 
 # funkcja do przetestowania
 def funkcja(a, b):

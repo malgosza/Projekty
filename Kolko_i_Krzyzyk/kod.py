13d3835237e475b4bbbe3ad75edd3d90f82bc3c0
Gosia <malgosza.niewiadomska@gmail.com>
2018-03-11 19:42:45 +0100

    zaimplementowana metoda ruchu

--- Kolko_i_Krzyzyk/kod.py
+++ Kolko_i_Krzyzyk/kod.py
@@ -1,12 +1,35 @@
 class poleGRY:
+
     def __init__(self):
         self.plansza=[]
-    def wypelnijPlansze(self):
-        for x in range(0,9):
+        self.KRZYZYK = 1
+        self.KOLKO = 2
+        self.czyTerazKrzyzyk = True
+
+        for x in range(0, 9):
             self.plansza.append(0)
-        print(self.plansza)
+
+    def piszPlansze(self):
+        for y in range(0,len(self.plansza)):
+            if(y%3==0 and y!=0):
+                print()
+            print(self.plansza[y], end='')
+
+    def ruchGracza(self, idx):
+        aktualnaFigura = self.KOLKO
+        if(self.czyTerazKrzyzyk == True):
+            aktualnaFigura = self.KRZYZYK
+            self.czyTerazKrzyzyk = False
+
+        self.plansza[idx]= aktualnaFigura
+
+    # zwraca 0 gdy gramy dalej
+    # 1 gdy wygral krzyzyk
+    # 2 gdy kolko
+    # 3 gdy remis
+    def wynikGry(self):
+        pass
+
+
 
 p=poleGRY()
-p.wypelnijPlansze()
-
-
